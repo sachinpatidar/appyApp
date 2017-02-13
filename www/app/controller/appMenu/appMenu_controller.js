cf7992ffbf61db74e3cd7770c4f64b801356f480
sachin patidar <sachin.p@cisinlabs.com>
2017-02-13 14:36:48 +0530

    check box

--- www/app/controller/appMenu/appMenu_controller.js
+++ www/app/controller/appMenu/appMenu_controller.js
@@ -7,67 +7,68 @@
     //  $scope.images = ["img/classprofile.png"];
     var a = [];
     var b = [];
-    httpServices.get("GetUserCategorySubCategory/" + localStorage.getItem('email') + "/" + localStorage.getItem('password') + "/" + localStorage.getItem('languageSelected')).then(function (response) {
+    httpServices.get("GetUserCategorySubCategory/" + localStorage.getItem('email') + "/" + localStorage.getItem('password') + "/" + localStorage.getItem('languageSelected')).then(function (response1) {
         //  console.log(response);
-        if (response.data.GetUserCatSubCatResult.length > 0) {
-            b = response.data.GetUserCatSubCatResult;
-            // $state.go('dashboard');
-        }
-    });
-    httpServices.get("GetCategorySubCategory/" + localStorage.getItem('languageSelected')).then(function (response) {
-        // console.log(response);
-        console.log(response);
-        if (response.data.GetCatSubCatResult.length > 0) {
-            a = response.data.GetCatSubCatResult;
-            // $state.go('dashboard');
-        }
-        $scope.val = [];
-        var cat = a[0].category;
-        var id = a[0].cateid;
-        var catsub = [];
-        var check = false;
-
-            a.map((i, j) => {
-                console.log(i.category + " " + cat);
-                if (i.category === cat) {
-                    //    for (var i = 0; i < b.length;)
-                    b.map((k, l) => {
-
-                        if (k.scateid === i.scateid) {
-                            check = true;
-                        }
-                    })
-                    // setTimeout(function () {
-                    catsub.push({ "scateid": i.scateid, "subcategory": i.subcategory, enableSub: check });
-                    //}, 40)
-
-                    check = false;
+        if (response1.data.GetUserCatSubCatResult.length > 0) {
+            b = response1.data.GetUserCatSubCatResult;
+            httpServices.get("GetCategorySubCategory/" + localStorage.getItem('languageSelected')).then(function (response) {
+                // console.log(response);
+                console.log(response);
+                if (response.data.GetCatSubCatResult.length > 0) {
+                    a = response.data.GetCatSubCatResult;
+                    // $state.go('dashboard');
                 }
-                else {
-                    $scope.val.push({ category: cat, subcategory: catsub, categoryId: id, enableCat: false });
-                    id = i.cateid;
-                    cat = i.category;
-                    check = false;
-                    b.map((k, l) => {
-                        if (k.scateid === i.scateid) {
-                            check = true;
-                        }
+                $scope.val = [];
+                var cat = a[0].category;
+                var id = a[0].cateid;
+                var catsub = [];
+                var check = false;
+
+                a.map((i, j) => {
+                    console.log(i.category + " " + cat);
+                    if (i.category === cat) {
+                        //    for (var i = 0; i < b.length;)
+                        b.map((k, l) => {
+
+                            if (k.scateid === i.scateid) {
+                                check = true;
+                            }
+                        })
+                        // setTimeout(function () {
+                        catsub.push({ "scateid": i.scateid, "subcategory": i.subcategory, enableSub: check });
+                        //}, 40)
+
+                        check = false;
+                    }
+                    else {
+                        $scope.val.push({ category: cat, subcategory: catsub, categoryId: id, enableCat: false });
+                        id = i.cateid;
+                        cat = i.category;
+                        check = false;
+                        b.map((k, l) => {
+                            if (k.scateid === i.scateid) {
+                                check = true;
+                            }
+                        })
+                        catsub = [];
+                        catsub.push({ "scateid": i.scateid, "subcategory": i.subcategory, enableSub: check })
+                    }
+
+                });
+                $scope.val.push({ category: cat, subcategory: catsub, categoryId: id, enableCat: false });
+                $scope.selectAllSub = function (val, $index) {
+                    console.log(val, $index);
+                    $scope.val[$index].subcategory.map((i, j) => {
+                        i.enableSub = val;
                     })
-                    catsub = [];
-                    catsub.push({ "scateid": i.scateid, "subcategory": i.subcategory, enableSub: check })
                 }
 
+                console.log($scope.val);
             });
-            $scope.val.push({ category: cat, subcategory: catsub, categoryId: id, enableCat: false });
-            $scope.selectAllSub = function (val, $index) {
-                console.log(val, $index);
-                $scope.val[$index].subcategory.map((i, j) => {
-                    i.enableSub = val;
-                })
-            }
-        
-        console.log($scope.val);
+            // $state.go('dashboard');
+        }
     });
+   
     $scope.Alrtcls = function () { Alepop.close(); }
     $scope.showAlert = function () {
         callAlert();
